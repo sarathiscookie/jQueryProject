1dd6ba1cab5f975301588e9504ac05fb5c3b1a29
Sarath <sarathiscookie@hotmail.com>
2016-07-06 20:03:32 +0530

    Added trim and each utility

--- jquery-core-utility-methods.php
+++ jquery-core-utility-methods.php
@@ -12,16 +12,26 @@
 <div class="container">
     <div clas="col-md-12">
         <div class="row">
-            <h4>Remove white space using trim</h4>
-            <h5>eg: ("    lots of extra whitespace    ")</h5>
+            <h4><b>1: Removes leading and trailing whitespace(trim)</b></h4>
+            <h5><b>eg: ("    lots of extra whitespace    ")</b></h5>
             <p id="trim"></p>
+        </div>
+    </div>
+    <div clas="col-md-12">
+        <div class="row">
+            <h4><b>2: Iterates over arrays and objects(each)</b></h4>
+            <h5><b>Array elements: "foo", "bar", "baz"</b></h5>
+            <ul id="list">
+            </ul>
         </div>
     </div>
 </div>
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://code.jquery.com/jquery-3.0.0.js"></script>
-<script src="https://code.jquery.com/jquery-migrate-3.0.0.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.js"></script>
+<!--<script src="https://code.jquery.com/jquery-3.0.0.js"></script>
+<script src="https://code.jquery.com/jquery-migrate-3.0.0.js"></script>-->
+
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
@@ -31,9 +41,11 @@
         var trim    = "    lots of extra whitespace    ";
         var trimmed = $.trim(trim);
         $("#trim").html("<b> Result : </b>"+trimmed);
+
+        $.each(["foo", "bar", "baz"], function(idx, val){
+            $("#list").append('<li id="'+idx+'">'+val+'</li>');
+        })
     });
-    /*http://learn.jquery.com/using-jquery-core/selecting-elements/*/
-    /*Create all the task for selecting-elements under this page*/
 </script>
 </body>
 </html>
